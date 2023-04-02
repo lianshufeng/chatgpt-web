d197bf1716dedfb8824935924cbaefb1d25aecd1
lianshufeng <251708339@qq.com>
2023-04-02 23:12:54 +0800

    submit

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: '../assets/avatar.jpg',
+      name: '人类',
+      description: 'Star on <a href="https://github.com/lianshufeng/chatgpt_api_demo" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
