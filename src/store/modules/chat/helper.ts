ea570940990e381aab0f2b784eebfaeb0ba9c011
lianshufeng <251708339@qq.com>
2023-04-12 01:14:10 +0800

    默认开启AI讲话

--- src/store/modules/chat/helper.ts
+++ src/store/modules/chat/helper.ts
@@ -7,7 +7,7 @@ export function defaultState(): Chat.ChatState {
   return {
     active: uuid,
     usingContext: true,
-    usingTTSContext: false,
+    usingTTSContext: true,
     history: [{ uuid, title: 'New Chat', isEdit: false }],
     chat: [{ uuid, data: [] }],
   }
